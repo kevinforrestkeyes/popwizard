304c88d3c2afd6d5c3d957b44578b4c153fcf657
kevinforrestkeyes <kevinforrestkeyes@gmail.com>
2019-11-12 17:56:02 -0800

    a bit of refactoring

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 import { getDepopProductsFromUser } from './lib/depop';
 import fetch from 'node-fetch';
 
-getDepopProductsFromUser(4835511)
-	.then((products) => {
-		const slug = products[0].slug;
-		const url = `https://popgoblin.kevinforrestkeyes.now.sh/https:/www.depop.com/products/${slug}`;
-		fetch(url)
-			.then(res => res.json())
-			.then(res => console.log(res))
-			.catch(function(err) {
-				console.error(err);
-			})
-	})
+import { getDepopProducts, updateDepopProducts, getUpdateStatus } from './lib/controller';
 
+const express = require('express');
+const app = express();
+
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', 'https://wjisk.netlify.com');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+})
+
+app.get('/', function (req, res) {
+	res.send('POPWIZARD')
+});
+
+app.get('/get-depop-products', async (req, res, next) => {
+	getDepopProducts()
+		.then((data) => res.send(data))
+		.catch((err) => console.error(err));
+});
+
+app.get('/update-depop-products', function (req, res) {
+	res.send({ status: 'updating products' });
+	updateDepopProducts();
+});
+
+app.get('/scrape-status', function (req, res) {
+	res.send(getUpdateStatus());
+})
+
+const PORT = process.env.PORT || 2222;
+
+app.listen(PORT, () => {
+	console.log(`app running on port ${PORT}`);
+});
